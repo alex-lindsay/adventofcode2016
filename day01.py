253dd78f1e8fed56279189373c313e8b0a7acaa5
Alex Lindsay <accounts@alex-lindsay.com>
2016-12-02 23:43:17 -0800

    tweaked algorithm for day 1 part 2

--- day01.py
+++ day01.py
@@ -1,14 +1,14 @@
 import math
 
+# instructionString = "R8, R4, R4, R8"
 instructionString = "L4, L1, R4, R1, R1, L3, R5, L5, L2, L3, R2, R1, L4, R5, R4, L2, R1, R3, L5, R1, L3, L2, R5, L4, L5, R1, R2, L1, R5, L3, R2, R2, L1, R5, R2, L1, L1, R2, L1, R1, L2, L2, R4, R3, R2, L3, L188, L3, R2, R54, R1, R1, L2, L4, L3, L2, R3, L1, L1, R3, R5, L1, R5, L1, L1, R2, R4, R4, L5, L4, L1, R2, R4, R5, L2, L3, R5, L5, R1, R5, L2, R4, L2, L1, R4, R3, R4, L4, R3, L4, R78, R2, L3, R188, R2, R3, L2, R2, R3, R1, R5, R1, L1, L1, R4, R2, R1, R5, L1, R4, L4, R2, R5, L2, L5, R4, L3, L2, R1, R1, L5, L4, R1, L5, L1, L5, L1, L4, L3, L5, R4, R5, R2, L5, R5, R5, R4, R2, L1, L2, R3, R5, R5, R5, L2, L1, R4, R3, R1, L4, L2, L3, R2, L3, L5, L2, L2, L1, L2, R5, L2, L2, L3, L1, R1, L4, R2, L4, R3, R5, R3, R4, R1, R5, L3, L5, L5, L3, L2, L1, R3, L4, R3, R2, L1, R3, R1, L2, R4, L3, L3, L3, L1, L2"
-
 instructions = instructionString.split(', ')
 
 orientation = 90
 x = 0
 y = 0
 
-visitedLocations = []
+visitedLocations = [(0,0)]
 answer2 = False
 
 for instruction in instructions:
@@ -18,23 +18,25 @@ for instruction in instructions:
 
     distance = int(instruction[1:])
 
-    if orientation == 0:
-        x += distance
-    if orientation == 90:
-        y += distance
-    if orientation == 180:
-        x -= distance
-    if orientation == 270:
-        y -= distance
+    xincr = math.trunc(math.sin(math.radians(orientation)))
+    yincr = math.trunc(math.cos(math.radians(orientation)))
 
     if not answer2:
-        try:
-            visitedLocations.index((x, y))
-            answer2 = "Day 1 second answer is: (%d, %d) %d" % (x, y, x + y)
-        except ValueError:
-            visitedLocations.append((x, y))
+        for i in range(1, distance+1):
+            x += xincr
+            y += yincr
+            try:
+                visitedLocations.index((x, y))
+                print "Here's the index:"
+                print visitedLocations.index((x, y))
+                print
+                print
+                print
+                answer2 = "Day 1 second answer is: (%d, %d) %d" % (x, y, x + y)
+            except ValueError:
+                visitedLocations.append((x, y))
 
-    print visitedLocations
+    print visitedLocations[-5:]
     print instruction, orientation, x, y
     print
 
