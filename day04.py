caac85990794eb7129c6892d85c8d79808cd42a2
Alex Lindsay <accounts@alex-lindsay.com>
2016-12-03 22:14:16 -0800

    Day 4 - Part 1

---
+++ day04.py
@@ -0,0 +1,34 @@
+import re
+inputFile = 'day04_input.txt'
+
+def createChecksum(char_count):
+    sort = [v[0] for v in sorted(char_count.items(), key=lambda(k,v): (-v,k))]
+    return ''.join(sort)[0:5]
+
+def processInput(inputFile):
+    valid_room_total = 0
+    input = open(inputFile, 'r')
+    for line in input:
+        char_count = {}
+        print line
+        matches = re.match(r"([-a-z]+)([0-9]+)\[([a-z]{5})\]", line)
+        print matches.group(1)
+        print matches.group(2)
+        print matches.group(3)
+        print
+
+        for char in matches.group(1):
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+        del char_count['-']
+        print char_count
+        checksum = createChecksum(char_count)
+        if checksum == matches.group(3):
+            valid_room_total += int(matches.group(2))
+
+    print valid_room_total
+
+
+print processInput(inputFile)
