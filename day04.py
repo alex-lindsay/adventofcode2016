c03186e8d2a296cbad9fa071b8b92d4267fbaf7b
Alex Lindsay <accounts@alex-lindsay.com>
2016-12-03 22:40:01 -0800

    Day 4 - Part 2

--- day04.py
+++ day04.py
@@ -10,12 +10,12 @@ def processInput(inputFile):
     input = open(inputFile, 'r')
     for line in input:
         char_count = {}
-        print line
+        # print line
         matches = re.match(r"([-a-z]+)([0-9]+)\[([a-z]{5})\]", line)
-        print matches.group(1)
-        print matches.group(2)
-        print matches.group(3)
-        print
+        # print matches.group(1)
+        # print matches.group(2)
+        # print matches.group(3)
+        # print
 
         for char in matches.group(1):
             if char in char_count:
@@ -23,10 +23,20 @@ def processInput(inputFile):
             else:
                 char_count[char] = 1
         del char_count['-']
-        print char_count
         checksum = createChecksum(char_count)
         if checksum == matches.group(3):
+            # print char_count
             valid_room_total += int(matches.group(2))
+            decoded = ""
+            for char in matches.group(1):
+                if char == '-':
+                    decoded += ' '
+                else:
+                    new_char = ((ord(char) - ord('a') + int(matches.group(2))) % 26) + ord('a')
+                    decoded += chr(new_char)
+            print 'decode: ', decoded
+            if decoded == "northpole object storage ":
+                print matches.group(2)
 
     print valid_room_total
 
